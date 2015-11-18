ee864b00313c61cbc6982d2ea31547badda98fb7
NebrasA <nalnemer5@gmail.com>
2015-11-18 13:41:06 -0500

    implement add remove feature for list of secrets

--- app/src/main/java/com/example/cse/secretsafe/lockerHomeActivity.java
+++ app/src/main/java/com/example/cse/secretsafe/lockerHomeActivity.java
@@ -2,12 +2,73 @@ package com.example.cse.secretsafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class lockerHomeActivity extends Activity{
 
+    private ListView listOfSecrets;
+    private  ArrayList<String> secrets;
+    private ArrayAdapter<String> secretsAdapter;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.locker_home);
+        //setupListViewListener();
+         listOfSecrets = (ListView)findViewById(R.id.bucket);
+        secrets = new ArrayList<>();
+
+
+        secretsAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,secrets);
+
+        listOfSecrets.setAdapter(secretsAdapter);
+        secrets.add("Secret One");
+        secrets.add("Secret Two!");
+        setupListViewListener();
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //Add secrets to the list
+    public void onAddSecret(View v){
+        EditText newSecret = (EditText)findViewById(R.id.newSecret);
+        String secretText = newSecret.getText().toString();
+        secretsAdapter.add(secretText);
+        newSecret.setText("");
+
+
+    }
+
+    // Attaches a long click listener to the listview
+    private void setupListViewListener() {
+        listOfSecrets.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        secrets.remove(pos);
+                        // Refresh the adapter
+                        secretsAdapter.notifyDataSetChanged();
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+
+                });
+    }
+
+
 }
